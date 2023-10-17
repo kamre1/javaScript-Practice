16a1b95a3db74f8059b59894d72705de6a066ee0
Md Kamre Alam <kamre8268@gmail.com>
2023-10-17 18:12:13 +0530

    Day_1

---
+++ Day_1/2_Variable.js
@@ -0,0 +1,7 @@
+console.log("This repl contains code for the Ultimate JavaScript course video no 2")
+var a = 82 // a contains 67
+console.log(a)
+a = "kamre"
+console.log(a)
+// let 8harry = 7 // Not allowed this will throw an error
+// let var = 7
---
+++ Day_1/3_const_let_var.js
@@ -0,0 +1,19 @@
+
+console.log("JavaScript tutorial 3: var, let and const")
+
+// var a = 34;
+// var a = "K"
+
+let b = "kamre";
+const author = "kamre"
+// author = 5 // Throws an error beacuse constant can't be changed
+//  b = 4;
+ const kamre = 0;
+ let c = null;
+ let d = undefined;
+ {
+    let b = 'this'
+    console.log(b)
+
+ }
+ console.log(b)
