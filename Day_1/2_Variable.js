2f62a7d093bf27bcde8df03811a416e083c451e3
Md Kamre Alam <kamre8268@gmail.com>
2023-10-17 21:35:58 +0530

    day2

--- Day_1/2_Variable.js
+++ Day_1/2_Variable.js
@@ -1,7 +1,9 @@
 console.log("This repl contains code for the Ultimate JavaScript course video no 2")
-var a = 82 // a contains 67
+var a = 82
+var b=2  // a contains 67
 console.log(a)
 a = "kamre"
 console.log(a)
+console.log(b)
 // let 8harry = 7 // Not allowed this will throw an error
 // let var = 7
